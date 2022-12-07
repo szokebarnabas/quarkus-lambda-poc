7d26cd3eaa882757b341a83ca4a77c0129fdc747
Barnabas Szoke <szokebarnabas85@gmail.com>
2022-12-07 21:16:40 +0100

    switch to jdk 17

--- src/main/java/poc/Product.java
+++ src/main/java/poc/Product.java
@@ -5,27 +5,4 @@ import io.quarkus.runtime.annotations.RegisterForReflection;
 import java.math.BigDecimal;
 
 @RegisterForReflection
-public class Product {
-    private final String id;
-    private final String name;
-    private final BigDecimal price;
-
-
-    public Product(String id, String name, BigDecimal price) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-}
+public record Product(String id, String name, BigDecimal price) {}
--- src/main/java/poc/ProductService.java
+++ src/main/java/poc/ProductService.java
@@ -11,6 +11,7 @@ public class ProductService {
     public List<Product> getAllProducts() {
         Product product1 = new Product(UUID.randomUUID().toString(), "Milk", BigDecimal.valueOf(450.5));
         Product product2 = new Product(UUID.randomUUID().toString(), "Bread", BigDecimal.valueOf(800.25));
-        return List.of(product1, product2);
+        Product product3 = new Product(UUID.randomUUID().toString(), "Egg", BigDecimal.valueOf(120.134));
+        return List.of(product1, product2, product3);
     }
 }
